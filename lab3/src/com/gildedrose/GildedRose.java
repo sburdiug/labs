dcbda17cdffca7a056bebb1b0c9dd339b5e4c1bb
sburdiug <sburdiug@gmail.com>
2024-12-18 01:20:11 +0100

    if else to switch
    Замінено if-else на switch-case для перевірки значення item.name
    Покращено читабельність і структуру коду

--- lab3/src/com/gildedrose/GildedRose.java
+++ lab3/src/com/gildedrose/GildedRose.java
@@ -33,19 +33,26 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
+            switch (item.name){
+                case "Aged Brie":
+                    if (item.sellIn < 0) {
                         Item.increaseQualityByOne(item);
-                } else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.sellIn < 0) {
                         item.quality = 0;
-                    } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            Item.decreaseQualityByOne(item);
-                        }
+                    }
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    if (item.sellIn < 0) {
+                        Item.decreaseQualityByOne(item);
+                    }
+                    break;
 
-                }
             }
         }
     }
-
 }
