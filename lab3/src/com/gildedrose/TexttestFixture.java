13686ae1295fc467065898711fcfa302d2cb6b3f
sburdiug <sburdiug@gmail.com>
2024-12-18 13:47:06 +0100

    TexttestFixture
    Додавання TexttestFixture та адаптація його під наш код
    тобто заміна методу Item на AgedBrie при потребі, і аналогічно для Sulfuras та Backstage

---
+++ lab3/src/com/gildedrose/TexttestFixture.java
@@ -0,0 +1,36 @@
+package com.gildedrose;
+
+public class TexttestFixture {
+    public static void main(String[] args) {
+        System.out.println("OMGHAI!");
+
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new AgedBrie(2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Sulfuras( 0, 80), //
+                new Sulfuras( -1, 80),
+                new Backstage( 15, 20),
+                new Backstage( 10, 49),
+                new Backstage(5, 49),
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 2;
+        if (args.length > 0) {
+            days = Integer.parseInt(args[0]) + 1;
+        }
+
+        for (int i = 0; i < days; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (Item item : items) {
+                System.out.println(item);
+            }
+            System.out.println();
+            app.updateQuality();
+        }
+    }
+
+}
